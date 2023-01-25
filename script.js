7268f7b2716f720b22a7f1cf3682e642fb2b37b8
Adam Cox <adamc567@gmail.com>
2023-01-25 10:20:23 -0600

    Basic JS and HTML

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
+
 let color = 'black';
+const pixel = document.querySelector('.sketchPad').children;
 const sketchPad = document.querySelector('.sketchPad');
+const clearBtn = document.querySelector('.clearButton')
 
 function divCreator(e) {
-    let divBlock
+let divBlock
+    sketchPad.innerHTML="";
     for (let i = 1; i <= e; i++) {
         divBlock = document.createElement('div');
+        divBlock.classList.add('pixel');
         sketchPad.appendChild(divBlock);
         divBlock.addEventListener('mouseover', function(a){
-           a.target.style.background = color;
-        })
-    }
-}
+                a.target.style.background = color;
+            })}}
+   
+
 function gridCreator(numberInput) {
    sketchPad.style.gridTemplateRows = (`repeat(${numberInput}, 1fr`);
    sketchPad.style.gridTemplateColumns = (`repeat(${numberInput}, 1fr)`);
+   console.log(numberInput)
 }
+function gridDestroyer() {
+    sketchPad.innerHTML=""
+    gridCreator(50);
+    divCreator(2500);
+}
+
+
+// // pen toggle
+// sketchPad.addEventListener('click', function() 
+// {
+// if (pen === 0) {
+//     pen = 1;
+//     // sketchPadDiv.addEventListener('mouseover', function(a){
+//     //     a.target.style.background = color;})
+
+//     console.log(pen)
+// } else {
+//     pen = 0;
+//     console.log(pen)
+//     return pen
+// }
+// })
 
 const numberInput = document.getElementById('inputNumber');
 numberInput.addEventListener('mousemove', function(){
@@ -27,23 +55,22 @@ let outPut = `${numberInput.value} x ${numberInput.value}`;
 numberInput.addEventListener('click', () => gridCreator(numberInput.value));
 numberInput.addEventListener('click', () => divCreator(numberInput.value * numberInput.value))
 
+clearBtn.addEventListener('click', () => gridDestroyer());
+
 const pixelColorPicker = document.getElementById('pixelColorPicker');
 pixelColorPicker.addEventListener('mouseout', () => color = pixelColorPicker.value)
 
-const blueBtn = document.getElementById('blue');
-blueBtn.addEventListener('click', () => color = 'blue')
-
-const redBtn = document.getElementById('red');
-redBtn.addEventListener('click', () => color ="red");
+const rainbowBtn = document.getElementById('rainbow');
+rainbowBtn.addEventListener('click', () => color = 'blue')
 
 const eraserBtn = document.getElementById('eraser');
 eraserBtn.addEventListener('click', () => color = backgroundColor.value);
 
 const backgroundColor = document.getElementById('colorPickerBackground')
 backgroundColor.addEventListener('mouseout', () => sketchPad.style.background= backgroundColor.value);
-//*funtions to add
-//eraser sizing
-//color-picker
-//pen toggle
-//sketchpad frame size
-//clear all
+
+// pixel.addEventListener('click', () =>
+// pixel.style.backgroundColor = color)
+
+window.onload = gridDestroyer();
+// const pixel = document.querySelector('.sketchPad').childr
